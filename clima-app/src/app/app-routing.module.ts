af71a1a8f46e8496c6840590e65316d302e48cad
Hamada <defjamvk@gmail.com>
2021-04-28 19:08:23 +0200

    Make Landpage(Presentation)
    Landpage(Presentation) to get start. Redirect from there to CalcComponent and the embeded History.

--- clima-app/src/app/app-routing.module.ts
+++ clima-app/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CalcComponent } from './calc/calc.component';
 import { LandComponent } from './land/land.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/' },
   { path: "", component: LandComponent },
+  { path: "calc", component: CalcComponent },
   { path: '**', redirectTo: '/' }
 ];
 
