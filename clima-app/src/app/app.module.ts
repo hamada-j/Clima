14181094879df60e5441eec80f9e6bd50fa23eba
Hamada <defjamvk@gmail.com>
2021-04-28 13:41:36 +0200

    Install Angular Material
    I will use Angular Material for UI.

--- clima-app/src/app/app.module.ts
+++ clima-app/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { HttpClientModule } from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
+
+// Component
 import { AppComponent } from './app.component';
 import { CalcComponent } from './calc/calc.component';
 import { HistoryComponent } from './history/history.component';
 import { LandComponent } from './land/land.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// UI (angular material)
+
+
+
 
 @NgModule({
   declarations: [
@@ -23,6 +30,7 @@ import { LandComponent } from './land/land.component';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
