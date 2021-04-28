a7245f574b31b054fde382feece3601787d97d32
Hamada <defjamvk@gmail.com>
2021-04-28 13:31:59 +0200

    Generate 3 component.
    Home: will be the presentation page
    Calc: hold the form and result
    History: will have a list table with all of result and request.

--- clima-app/src/app/app.module.ts
+++ clima-app/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalcComponent } from './calc/calc.component';
+import { HistoryComponent } from './history/history.component';
+import { LandComponent } from './land/land.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalcComponent,
+    HistoryComponent,
+    LandComponent
   ],
   imports: [
     BrowserModule,
