a6034d1b7644bcbcc58833e146b807ad90747cdd
Hamada <defjamvk@gmail.com>
2021-04-28 13:53:59 +0200

    add BrowserAnimationsModule
    add BrowserAnimationsModule to fix the animation platform.

--- clima-app/src/app/app.module.ts
+++ clima-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -24,6 +25,7 @@ import {MatTabsModule} from '@angular/material/tabs';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
