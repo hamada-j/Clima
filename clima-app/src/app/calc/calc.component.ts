e7a78500a5c4d1369b6d58369cf6fd10a496de19
Hamada <defjamvk@gmail.com>
2021-04-29 09:28:02 +0200

    Add and Remove Inputs
    Add:
    
    - User can Add Inpus till 50 (beacouse of response in Desck-top devaice, maybe for phone and tablet shut be less)
    - Each Input has a Unique ID and this id is push to an Array of Id's.
    - If the Array of Id's is with length more the cero, then user can remove the last element has created.

--- clima-app/src/app/calc/calc.component.ts
+++ clima-app/src/app/calc/calc.component.ts
@@ -1,5 +1,7 @@
 import { Component, ElementRef, Inject,  Renderer2, ViewChild } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+
+import { generateId } from '../utils/generateId';
 @Component({
   selector: 'app-calc',
   templateUrl: './calc.component.html',
@@ -16,9 +18,28 @@ export class CalcComponent  {
 
 
 
-    addElementMethod(e){console.log(e)}
+     addElementMethod(e){
 
-    removeElementMethod(e){console.log(e)}
+      if (this.arrIds.length < 50 ) {
+        let idInput = generateId();
+        this.toAddElement.nativeElement.insertAdjacentHTML('beforeend', `<input type="text" id="${idInput}" ngModel />`);
+        this.arrIds.push(idInput);
+        console.log(this.arrIds);
+      }
+
+    }
+
+  removeElementMethod(e){
+
+    if (this.arrIds.length > 0 ) {
+      let element = this.document.getElementById('inputUser');
+      let id = element.lastChild.id
+      this.arrIds.splice(this.arrIds.indexOf(id), 1);
+      element.removeChild(element.lastChild);
+      console.log(this.arrIds)
+    }
+  }
+
 
     onSubmit(formValues) {console.log(formValues)}
 
